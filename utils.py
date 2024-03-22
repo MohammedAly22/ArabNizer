d361028217dc328ce74b1aa58c09534d5aeb59da
Mohammed Aly <90681796+MohammedAly22@users.noreply.github.com>
2024-03-22 23:26:07 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
+import re
 from transformers import pipeline
+from nltk import word_tokenize
 
 
 pipe = pipeline('token-classification', 'mohammedaly22/arabnizer-xlmr-panx-ar')
@@ -7,8 +9,20 @@ class_mapper = {
     'PER': 'شخص',
     'ORG': 'مؤسسة',
     'LOC': 'عنوان',
+    'X': ''
 }
 
+
+
+def get_all_words_and_entities(text, words, entities):
+    word_entity_mapper = {word: entity for word, entity in zip(words, entities)}
+    all_tokens = word_tokenize(text)
+
+    all_words = [token for token in all_tokens]
+    all_entities = ["X" if token not in words else word_entity_mapper[token] for token in all_tokens]
+
+    return all_words, all_entities
+    
 
 def generate_highlighted_word(word, entity):
     highlight_word = f' <span class="highlight-{entity}"> ' + word + f' <span class="highlight-inner-{entity}"> ' + class_mapper[entity] + " </span> " + " </span> "
@@ -22,7 +36,6 @@ def generate_highlighted_text(words, entities):
     for word, entity in zip(words, entities):
         highlight_word = generate_highlighted_word(word, entity)
         rendered_text += highlight_word
-        # rendered_text += ' <br> '
 
     rendered_text += ' </div>'
 
